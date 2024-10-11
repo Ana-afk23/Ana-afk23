157c79903fb4bb62848da571e7027b5fddd74789
Ana Rafaely <aninhaaoliveiraa61@gmail.com>
2024-10-11 14:43:57 -0300

    adicionei dois novos recursos na api

--- api/Program.cs
+++ api/Program.cs
@@ -7,7 +7,8 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
         var app = builder.Build();
 
-        app.MapGet("/", () => "Hello World!");
+        app.MapGet("/", () => "Página Principal");
+        app.MapGet("/Produtos", () => "Lista de Produtos");
 
         app.Run();
     }
